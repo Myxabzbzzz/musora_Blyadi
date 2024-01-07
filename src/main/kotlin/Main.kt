847ffb62c39280dfdaf01f1c076666c62a9bdd0c
Myxabzbzzzz <m.elibayev@student.inha.uz>
2024-01-08 03:47:42 +0500

    ny blyad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,56 +1,147 @@
 
 
 
-//fun reverseWord( word: String ): String {
-//    val charArray = word.toCharArray()
-//    for (i in 0 until charArray.size / 2){
-//        val temp = charArray[i]
-//        charArray[i] = charArray[charArray.size - 1 - i]
-//        charArray[charArray.size - 1 - i] = temp
-//    }
-//return String(charArray)
-//}
-fun main(args: Array<String>) {
+fun reverseWord( word: String ): String {
+    val charArray = word.toCharArray()
+    for (i in 0..<charArray.size / 2){
+        val temp = charArray[i]
+        charArray[i] = charArray[charArray.size - 1 - i]
+        charArray[charArray.size - 1 - i] = temp
+    }
+    return String(charArray)
+}
+
+fun quickSort(array: IntArray, low: Int, high: Int, descending: Boolean = false) {
+    if (low < high) {
+        val pivotIndex = delitelArraya(array, low, high, descending)
+        quickSort(array, low, pivotIndex - 1, descending)
+        quickSort(array, pivotIndex + 1, high, descending)
+    }
+}
+
+fun delitelArraya(array: IntArray, low: Int, high: Int, descending: Boolean = false): Int {
+    val pivot = array[high]
+    var i = low - 1
+
+    for (j in low..<high) {
+        val compareResult = if (descending) array[j] >= pivot else array[j] <= pivot
+
+        if (compareResult) {
+            i++
+            pomenyashka(array, i, j)
+        }
+    }
+
+    pomenyashka(array, i + 1, high)
+    return i + 1
+}
+
+fun pomenyashka(array: IntArray, i: Int, j: Int) {
+    val temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+}
+
+
+
+
+
+    fun main(args: Array<String>) {
     var showMenu = true
     while (showMenu){
-        println("Hello epta! koroche, choose your program \n1. Program to reverse any word that you input \n2.Program to sort array from lowest to greatest \n3. Program to do pochti the same huinya tolko vice versa \n4. program to draw sequence_1 \n5. program to draw sequence_2 \n6. console calculator naxui \n7. chislo iz fibonnaccccccci suka pedik naxui on ix sdelal pidaraz" )
-        var prg = readln()
+
+        println("Hello epta! koroche, choose your program ")
+        println("1. Program to reverse any word that you input")
+        println("2. Program to sort array from lowest to greatest i vice versa")
+        println("3. program to draw sequence_1")
+        println("4. program to draw sequence_2")
+        println("5. console calculator naxui")
+        println("6. chislo iz fibonnaccccccci suka pedik naxui on ix sdelal pidaraz")
 
 
-        when(prg) {
-            "1" -> println("введите слово мистер пидор: ")
+        var program = readln()
 
 
-            "2" -> println("pawer naxuy javad!")
-            "3" -> println("pawer naxuy shama!")
-            "4" -> println("pawer naxuy mama!")
-            "5" -> println("pawer naxuy papa!")
-            "6" -> println("pawer naxuy shar!")
+        when(program) {
+
+            "1" -> {
+
+                println("vvedite slovo mister pider: ")
+
+                val inputWord = readln()
+
+                if (inputWord.isNotBlank()) {
+                    val reversedWord = reverseWord(inputWord)
+                    println("reversed word : $reversedWord")
+                } else {
+                    println("pashel naxuy.")
+                }
+
+            }
+            "2" -> {
+
+                print("Array razmer huini suda: ")
+                val size = readlnOrNull()?.toIntOrNull()
+
+                if (size != null && size > 0) {
+                    val array = IntArray(size)
+
+
+                    for (i in 0..<size) {
+
+                        print("teper xuini element #$i: ")
+
+                        array[i] = readlnOrNull()?.toIntOrNull() ?: 0
+                    }
+
+                    println("vo kakoy massiv vishel: ${array.joinToString()}")
+
+
+                    quickSort(array, 0, size - 1, descending = false)
+
+                    println("na suka svoy massiv takoy: ${array.joinToString()}")
+
+
+                    quickSort(array, 0, size - 1, descending = true)
+
+                    println("na suka svoy massiv sekoi: ${array.joinToString()}")
+                } else {
+
+                    println("govno.")
+
+                }
+
+            }
+
+            "3" -> {println("vvedi kolichestvo strok v stairse!")
+
+
+
+
+            }
+            "4" -> {println("pawer naxuy mama!")}
+            "5" -> {println("pawer naxuy papa!")}
+            "6" -> {println("pawer naxuy shar!")}
 
 
 
 
 
         }
+
+
         println("bazara net, hochesh druguyu pro gu vibrat? pishi 1 esli da i idi naxuy esli net")
+
        val answer = readln()
 
-        val mneNadoMenushka = answer?.toIntOrNull()
-        if (mneNadoMenushka != 1){
+
+        if (answer != "1"){
             showMenu = false
         }
 
     }
 
-    fun reverseWord( word: String ): String {
-        val charArray = word.toCharArray()
-        for (i in 0 until charArray.size / 2){
-            val temp = charArray[i]
-            charArray[i] = charArray[charArray.size - 1 - i]
-            charArray[charArray.size - 1 - i] = temp
-        }
-        return String(charArray)
-    }
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
