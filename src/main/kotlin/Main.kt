fd64329a637b54fa3fa4e65b01576d0d2ee4fd59
Myxabzbzzzz <m.elibayev@student.inha.uz>
2024-01-11 00:41:34 +0500

    ny vse

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,21 @@
+import kotlin.math.pow
 
 
 
-fun reverseWord( word: String ): String {
+fun reverseWord(word: String): String {
+
+
     val charArray = word.toCharArray()
-    for (i in 0..<charArray.size / 2){
+
+
+    for (i in 0..<charArray.size / 2) {
+
         val temp = charArray[i]
+
         charArray[i] = charArray[charArray.size - 1 - i]
         charArray[charArray.size - 1 - i] = temp
     }
+
     return String(charArray)
 }
 
@@ -24,6 +32,7 @@ fun delitelArraya(array: IntArray, low: Int, high: Int, descending: Boolean = fa
     var i = low - 1
 
     for (j in low..<high) {
+
         val compareResult = if (descending) array[j] >= pivot else array[j] <= pivot
 
         if (compareResult) {
@@ -37,112 +46,267 @@ fun delitelArraya(array: IntArray, low: Int, high: Int, descending: Boolean = fa
 }
 
 fun pomenyashka(array: IntArray, i: Int, j: Int) {
+
     val temp = array[i]
+
     array[i] = array[j]
     array[j] = temp
 }
 
 
+fun plusMinusUmnojitDelit(operation: Int) {
 
+    print("birinchi chislo: ")
+    val num1 = readlnOrNull()?.toDoubleOrNull()
 
+    print("ikkinchi chislo: ")
+    val num2 = readlnOrNull()?.toDoubleOrNull()
 
-    fun main(args: Array<String>) {
-    var showMenu = true
-    while (showMenu){
+    if (num1 != null && num2 != null) {
 
-        println("Hello epta! koroche, choose your program ")
-        println("1. Program to reverse any word that you input")
-        println("2. Program to sort array from lowest to greatest i vice versa")
-        println("3. program to draw sequence_1")
-        println("4. program to draw sequence_2")
-        println("5. console calculator naxui")
-        println("6. chislo iz fibonnaccccccci suka pedik naxui on ix sdelal pidaraz")
+        when (operation) {
 
+            1 -> println("result: ${num1 + num2}")
 
-        var program = readln()
+            2 -> println("result: ${num1 - num2}")
 
+            3 -> println("result: ${num1 * num2}")
 
-        when(program) {
+            4 -> {
+                if (num2 != 0.0) {
 
-            "1" -> {
+                    println("result: ${num1 / num2}")
 
-                println("vvedite slovo mister pider: ")
-
-                val inputWord = readln()
-
-                if (inputWord.isNotBlank()) {
-                    val reversedWord = reverseWord(inputWord)
-                    println("reversed word : $reversedWord")
-                } else {
-                    println("pashel naxuy.")
-                }
-
-            }
-            "2" -> {
-
-                print("Array razmer huini suda: ")
-                val size = readlnOrNull()?.toIntOrNull()
-
-                if (size != null && size > 0) {
-                    val array = IntArray(size)
-
-
-                    for (i in 0..<size) {
-
-                        print("teper xuini element #$i: ")
-
-                        array[i] = readlnOrNull()?.toIntOrNull() ?: 0
-                    }
-
-                    println("vo kakoy massiv vishel: ${array.joinToString()}")
-
-
-                    quickSort(array, 0, size - 1, descending = false)
-
-                    println("na suka svoy massiv takoy: ${array.joinToString()}")
-
-
-                    quickSort(array, 0, size - 1, descending = true)
-
-                    println("na suka svoy massiv sekoi: ${array.joinToString()}")
                 } else {
 
-                    println("govno.")
+                    println("ya sha tebya na nol podelyu chort.")
 
                 }
-
             }
-
-            "3" -> {println("vvedi kolichestvo strok v stai rse!")
-
-
-
-
-            }
-            "4" -> {println("pawer naxuy mama!")}
-            "5" -> {println("pawer naxuy papa!")}
-            "6" -> {println("pawer naxuy shar!")}
-
-
-
-
-
         }
+    } else {
 
-
-        println("bazara net, hochesh druguyu pro gu vibrat? pishi 1 esli da i idi naxuy esli net")
-
-       val answer = readln()
-
-
-        if (answer != "1"){
-            showMenu = false
-        }
+        println("kak pedik pishesh.")
 
     }
-
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
+
+fun procent() {
+
+    print("birinchi chislo: ")
+    val num1 = readlnOrNull()?.toDoubleOrNull()
+
+
+    print("ikkinchi chislo: ")
+    val num2 = readlnOrNull()?.toDoubleOrNull()
+
+
+    if (num1 != null && num2 != null) {
+
+        if (num2 != 0.0) {
+            println("result: ${num1 % num2}")
+        } else {
+            println("ya sha tebya na nol podelyu chort.")
+        }
+
+
+    } else {
+        println("kak pedik pishesh.")
+    }
+}
+
+fun ostepen() {
+
+    print("birinchi chislo: ")
+    val base = readlnOrNull()?.toDoubleOrNull()
+
+    print("ikkinchi chislo: ")
+    val exponent = readlnOrNull()?.toDoubleOrNull()
+
+    if (base != null && exponent != null) {
+        println("result: ${base.pow(exponent)}")
+    } else {
+        println("kak pedik pishesh.")
+    }
+
+}
+
+fun factorial() {
+
+    print("faktorial xochesh? pishi chislo: ")
+    val num = readlnOrNull()?.toIntOrNull()
+
+    if (num != null && num >= 0) {
+
+        var factorial = 1
+
+        for (i in 1..num) {
+            factorial *= i
+        }
+
+        println("vot tebe suka tvoi factorial $num: $factorial")
+    } else {
+        println("pawel naxuy pishi normalno eban.")
+    }
+
+    fun main(args: Array<String>) {
+
+        var showMenu = true
+        while (showMenu) {
+
+            println("Hello epta! koroche, choose your program ")
+            println("1. Program to reverse any word that you input")
+            println("2. Program to sort array from lowest to greatest i vice versa")
+            println("3. program to draw sequence_1")
+            println("4. program to draw sequence_2")
+            println("5. console calculator naxui")
+            println("6. chislo iz fibonnaccccccci suka pedik naxui on ix sdelal pidaraz")
+
+            val program = readln()
+
+            when (program) {
+                "1" -> {
+
+                    println("vvedite slovo mister pider: ")
+                    val inputWord = readln()
+
+                    if (inputWord.isNotBlank()) {
+
+                        val reversedWord = reverseWord(inputWord)
+                        println("reversed word : $reversedWord")
+
+                    } else {
+
+                        println("pashel naxuy.")
+
+                    }
+                }
+
+                "2" -> {
+
+                    print("Array razmer huini suda: ")
+
+                    val size = readlnOrNull()?.toIntOrNull()
+
+                    if (size != null && size > 0) {
+
+                        val array = IntArray(size)
+
+                        for (i in 0..<size) {
+                            print("teper xuini element #$i: ")
+                            array[i] = readlnOrNull()?.toIntOrNull() ?: 0
+                        }
+
+                        println("vo kakoy massiv vishel: ${array.joinToString()}")
+                        quickSort(array, 0, size - 1, descending = false)
+
+                        println("na suka svoy massiv takoy: ${array.joinToString()}")
+                        quickSort(array, 0, size - 1, descending = true)
+
+                        println("na suka svoy massiv sekoi: ${array.joinToString()}")
+
+                    } else {
+
+                        println("govno.")
+
+                    }
+                }
+
+                "3" -> {
+                    print("skolko zvezd-pyozd tebe nado?: ")
+
+                    val size = readlnOrNull()?.toIntOrNull() ?: 0
+
+                    for (i in 1..size) {
+
+                        for (j in 0..<i) {
+
+                            print("*")
+
+                        }
+
+                        println()
+
+                    }
+                }
+
+                "4" -> {
+
+                    print("skolko zvezd-pyozd tebe nado?: ")
+
+                    val size = readlnOrNull()?.toIntOrNull() ?: 0
+
+                    for (i in size downTo 1) {
+
+                        for (j in 0..<i) {
+                            print("*")
+
+                        }
+
+                        println()
+
+                    }
+                }
+
+                "5" -> {
+
+                    while (true) {
+
+                        println("che xochesh?:")
+                        println("1. pulus")
+                        println("2. minus")
+                        println("3. umnojeniye")
+                        println("4. deleniye")
+                        println("5. deleniye na percent")
+                        println("6. power to chtoto")
+                        println("7. factorial")
+                        println("0. viyti v druguyu menushka")
+                        print("vibirai: ")
+
+                        when (val choice = readlnOrNull()?.toIntOrNull()) {
+
+                            0 -> {
+
+                                println("sha verneshsa bratishka.")
+                                break
+
+                            }
+
+                            in 1..4 -> choice?.let { plusMinusUmnojitDelit(it) }
+                            5 -> procent()
+                            6 -> ostepen()
+                            7 -> factorial()
+                            else -> println("pawel naxuy pishi kak chelovek.")
+                        }
+
+                        println()
+                    }
+                }
+
+                "6" -> println("pawer naxuy shar!")
+            }
+            println("bazara net, hochesh druguyu pro gu vibrat? pishi 1 esli da i idi naxuy esli net")
+
+            val answer = readln()
+
+            if (answer != "1") {
+
+                showMenu = false
+            }
+        }
+        // Try adding program arguments via Run/Debug configuration.
+        // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+        println("Program arguments: ${args.joinToString()}")
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
